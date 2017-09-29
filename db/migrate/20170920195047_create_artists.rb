ef7f11e7d599e86539f81898101f2d27e2019008
Brian Cohen Falah <briancf@gmail.com>
2017-09-29 13:21:03 -0300

    Add missing index to artists name column

--- db/migrate/20170920195047_create_artists.rb
+++ db/migrate/20170920195047_create_artists.rb
@@ -1,7 +1,7 @@
 class CreateArtists < ActiveRecord::Migration[5.1]
   def change
     create_table :artists do |t|
-      t.string :name, null: false, unique: true
+      t.string :name, null: false
       t.text :biography
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170928022219) do
+ActiveRecord::Schema.define(version: 20170929161821) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20170928022219) do
     t.text "biography"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_artists_on_name", unique: true
   end
 
   create_table "genres", force: :cascade do |t|
